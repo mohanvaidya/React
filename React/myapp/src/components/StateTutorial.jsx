87e4aae70b0a6483cf43b857a621128cb7fde837
Mohan Vaidya <131249018+mohanvaidya@users.noreply.github.com>
2023-09-20 16:24:47 +0530

    Update StateTutorial.jsx

--- React/myapp/src/components/StateTutorial.jsx
+++ React/myapp/src/components/StateTutorial.jsx
@@ -1,1 +1,34 @@
 
+import React, { Component } from 'react'
+
+ class StateTutorial extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = { 
+       message:'subscribe to my channel',
+         sub:'Subscribe'
+      };
+    };
+    Buttonchange=()=>{
+        this.setState({
+
+            message:'thank you for subscribing please hit the bell icon',
+            sub:'subscribed'
+        });
+    }
+
+  render() {
+    return (
+      <div><h1>stateTutorial</h1>
+
+      <p>{this.state.message} </p>
+
+      <button onClick={this.Buttonchange}>{this.state.sub}</button><br />
+      
+      </div>
+    )
+  }
+}
+
+export default StateTutorial;
